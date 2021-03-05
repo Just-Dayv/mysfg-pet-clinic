0792c1ceb4c7562594c69f805c27d6a89a70a68d
David <iyanuoluwaopa@gmail.com>
2021-03-05 22:45:47 +0100

    Added id to models via base entity

--- pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.sprin5.tutorials.mysfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.sprin5.tutorials.mysfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
