a5c8afe90354eaf71354aac9c50aed75d3536cc5
David <iyanuoluwaopa@gmail.com>
2021-03-06 22:22:56 +0100

    Added Index page and Controller

---
+++ pet-clinic-web/src/main/java/com/sprin5/tutorials/mysfgpetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.sprin5.tutorials.mysfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
