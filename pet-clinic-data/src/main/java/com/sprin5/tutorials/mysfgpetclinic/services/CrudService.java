51d702c17acbd84328fe22a86a810c8987877620
David <iyanuoluwaopa@gmail.com>
2021-03-06 16:16:04 +0100

    Refactored services via Crud services

---
+++ pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/services/CrudService.java
@@ -0,0 +1,11 @@
+package com.sprin5.tutorials.mysfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+    T findById (ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(ID id);
+}
