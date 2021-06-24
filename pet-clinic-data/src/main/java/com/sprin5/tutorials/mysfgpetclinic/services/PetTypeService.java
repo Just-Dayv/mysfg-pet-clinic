35351fc5e3439f50001b60e49d030f8f95687125
David <iyanuoluwaopa@gmail.com>
2021-06-24 23:30:24 +0100

    Added Owner.java properties

---
+++ pet-clinic-data/src/main/java/com/sprin5/tutorials/mysfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.sprin5.tutorials.mysfgpetclinic.services;
+
+import com.sprin5.tutorials.mysfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
